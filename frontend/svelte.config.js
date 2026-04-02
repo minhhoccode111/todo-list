9e518cd004acbcae0f5351e596a30ead37c970de
minhhoccode111 <minhhoccode111@gmail.com>
2026-04-02 22:42:17 +0700

    feat: cicd

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,17 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/todo-list'
+		}
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
